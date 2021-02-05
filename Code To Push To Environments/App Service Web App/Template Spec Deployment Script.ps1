cc6b9a12220a4a532723fe399f5e0bad4cf2112c
Paul Fisher <pfisher@microsoft.com>
2021-02-05 15:07:21 -0500

    removed subscription id

--- Code To Push To Environments/App Service Web App/Template Spec Deployment Script.ps1
+++ Code To Push To Environments/App Service Web App/Template Spec Deployment Script.ps1
@@ -1,4 +1,4 @@
-param($specrgroupname="ghgovenv", $preprodrgroupname="ghgovenvpreprod", $prodrgroupname="ghgovenvprod", $appServicePlanSpecName="GHGovDemoAppServicePlan", $appServiceSpecName="GHGovDemoAppService", $version="1.0", $location="eastus2", $subscriptionid="4d34293e-e544-4865-887d-b49c169bf080")
+param($specrgroupname="ghgovenv", $preprodrgroupname="ghgovenvpreprod", $prodrgroupname="ghgovenvprod", $appServicePlanSpecName="GHGovDemoAppServicePlan", $appServiceSpecName="GHGovDemoAppService", $version="1.0", $location="eastus2", $subscriptionid="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
 
 $planid = $(az ts show --name $appServicePlanSpecName --resource-group $specrgroupname --version $version --query "id")
 az deployment group create --resource-group $preprodrgroupname --template-spec $planid --parameters name="asplan-topreprodghgovenv" location=$location
